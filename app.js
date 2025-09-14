a660a84788cd345d14709abdecb7b02639c0d7cd
Rujhaan Srivastava <rujhaansrivastava21@gmail.com>
2025-09-14 13:19:37 +0530

    First commit - Tic Tac Toe project

---
+++ app.js
@@ -0,0 +1,72 @@
+let boxes= document.querySelectorAll(".box");
+let resetbtn= document.querySelector("#resetbtn");
+let newgamebtn= document.querySelector("#new-btn");
+let msgcontainer= document.querySelector(".msg-container");
+let msg= document.querySelector('#msg');
+let turnO= true;
+const winpatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+]
+
+const resetgame=()=>{
+     turnO= true;
+    enableboxes();
+     msgcontainer.classList.add("hide");
+
+}
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(turnO){
+            box.innerText="O";
+            turnO=false;
+        }
+        else{
+            box.innerText="X";
+              turnO= true;
+        }
+        box.disabled=true;
+        checkwinner();
+    });
+});
+ const disableboxes=()=>{
+    for(box of boxes){
+        box.disabled=true;
+    }
+ }
+ const enableboxes=()=>{
+    for(box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+ }
+const showWinner=(Winner)=>{
+    msg.innerText=`Congratuations, Winner is ${Winner}`;
+    msgcontainer.classList.remove("hide");
+    disableboxes();
+}
+const checkwinner=()=>{
+    for(let pattern of winpatterns){
+         console.log(pattern[0],pattern[1],pattern[2]);
+
+     let pos1=boxes[pattern[0]].innerText;
+     let pos2=boxes[pattern[1]].innerText;
+     let pos3=boxes[pattern[2]].innerText;
+
+     if(pos1!=""&&pos2!=""&&pos3!=""){
+        if(pos1===pos2&&pos2===pos3){
+            showWinner(pos1);
+        }
+     }
+   
+}
+
+};  
+newgamebtn.addEventListener("click",resetgame);
+resetbtn.addEventListener("click",resetgame);
